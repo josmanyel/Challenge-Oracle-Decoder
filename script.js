0c26ced23b48bf5043ea7e985bc2663be6a4a5b7
JoséManuel <josmanpaca@gmail.com>
2023-01-26 23:56:38 -0400

    Terminada la funcionalidad en JS, ahora a los detalles

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ const mensaje = document.querySelector(".mensaje");
 // `La letra "o" es convertida para "ober"`
 // `La letra "u" es convertida para "ufat"`
 
-function btnEncriptar () {
-
+function btnEncriptar(){
     const textoEncriptado = encriptar(textArea.value);
     mensaje.value = textoEncriptado;
     textArea.value = "";
@@ -16,12 +15,12 @@ function btnEncriptar () {
     
 }
 
-function encriptar(stringEncriptada) {
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
@@ -35,13 +34,13 @@ function btnDesencriptar () {
     
 }
 
-function desencriptar(stringDesencriptada) {
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringDesencriptada.includes(matrizCodigo[i][1])) {
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0]);
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
         }
     }
     return stringDesencriptada;
